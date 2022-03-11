11d89b393b24e93fc4516b771061dd0692129af7
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-11 16:23:58 +0100

    esercizio tre

--- terzo snack/script.js
+++ terzo snack/script.js
@@ -8,10 +8,35 @@
  */
 
 
-let stringhe = ["ciccio", "pasticcio", "bella", "storia", "scuola"];
+
+let stringhe = ["CicCCio", "pastiCciO", "bELLA", "storIA", "sCuOla"]
+
+
+/* creo una copia dell'array "stringhe" ma con delle modifiche */
+let newStringa = stringhe.map((Element) =>{
+    return Element.charAt(0).toUpperCase() +    /* ritorna l'elemento, con però la lettera in posizione 0 maiuscola  PIU' */
+     Element.substring(1).toLowerCase()         /* il resto della stringa dell'elemento a partire dalla posizione 1 in minuscolo */
+})
+
+console.log(newStringa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let stringhe = ["ciccio", "pasticcio", "bella", "storia", "scuola"];
 
 let newStringhe = stringhe.map((Element) =>{
     return  Element.toUpperCase() 
 })
 
-console.log(newStringhe)
+console.log(newStringhe) */
