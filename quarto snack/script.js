7b6394f804d916d2a30473b61f6931e381f3b709
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-11 16:33:11 +0100

    quarto snack

--- quarto snack/script.js
+++ quarto snack/script.js
@@ -22,6 +22,10 @@ let animali = [
     { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
     { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
     { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: "tacchino", famiglia: "fasianidi", classe: "uccelli"},
+    { nome: "leopardo", famiglia: "felini", classe:"mammiferi"},
+    { nome: "tigre", famiglia: "felini", classe:"mammiferi"},
+    { nome: "piccione", famiglia: "fasianidi", classe:"uccelli"},
 ];
 
 
@@ -32,5 +36,15 @@ let animali = [
         else return false
     })
 
+    let pennuti = animali.filter((element) => {
+        if(element.classe == "uccelli") return true
+    })
 
-console.log(animaliMammiferi)
+    let micioni = animali.filter((element) => {
+        if(element.famiglia == "felini") return true
+    })
+
+
+console.table(micioni);
+console.table(pennuti);
+console.table(animaliMammiferi);
